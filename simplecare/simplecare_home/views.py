e0dfad651c75b5139904d023eba6ea084d7152dc
PlibbytheNoodle <33815744+PlibbytheNoodle@users.noreply.github.com>
2019-09-22 01:55:03 -0700

    changed one line of code to append css access

--- simplecare/simplecare_home/views.py
+++ simplecare/simplecare_home/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 
 def simplecare(request):
     return render(request, 'simplecare_home/simplecare_input_text.html')
+    file = request.POST.get('pdflink')
+    return file
