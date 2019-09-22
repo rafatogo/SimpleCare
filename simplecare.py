75f3e6297fe4f2458b7fb82daca1e8e8aafdce3b
mini-mini-milo <53094803+mini-mini-milo@users.noreply.github.com>
2019-09-22 09:58:30 -0700

    Update simplecare.py

--- simplecare.py
+++ simplecare.py
@@ -13,97 +13,122 @@ Created on Sat Sep 21 12:03:57 2019
     #if search phrase in textfile:
         #print line(s) 
 """
-def dataExtract(fileName):
-    dataList = {}
-    txt = open(fileName, encoding = "utf8")
-    allLines = txt.readlines()
-    s = allLines[1]
-    s = s.strip(": ")
-    dataList["Plan Name"] = s[:-1]
-    deductibleFound = False
-    deductibleLocation = -1
-    outOfPocketLocation = -1
-    copayLocation = -1
-    count = 0
-    notCoveredLine = 0
-    coveredLine = 0
-    def calcPremium(deductible):
-    
-    #calculates the premium relative to deductible
-    
-        deductible = int((dataList["Deductible"].split()[0][1:]).replace(',', ''))
-        if deductible < 700:
-            dataList["Premium Level"] = "Higher Premium Level"
-        elif deductible > 1400:
-            dataList["Premium Level"] = "Lower Premium Level"
-        else:
-            dataList["Premium Level"] = "Medium Premium Level"
-    
-    for line in allLines:
-        if "Plan Type" in line:
-            strList = line.split()
-            planType = strList[-1]
-            if planType == "HMO":
-                dataList["Do I have to stay in network?"] = "Yes"
-                dataList["Do I need a referral to see a specialist?"] = "Yes"
-            elif planType == "PPO":
-                dataList["Do I have to stay in network?"] = "No"
-                dataList["Do I need a referral to see a specialist?"] = "No"
-            elif planType == "EPO":
-                dataList["Do I have to stay in network?"] = "Yes"
-                dataList["Do I need a referral to see a specialist?"] = "No"
-            elif planType == "POS":
-                dataList["Do I have to stay in network?"] = "No"
-                dataList["Do I need a referral to see a specialist?"] = "Yes"
+easyCompareList = list()
+
+while True:
+    fileName = input('File Name: ')
+    if fileName != 'done':
+        dataList = {}
+        txt = open(fileName, encoding = "utf8")
+        allLines = txt.readlines()
+        s = allLines[1]
+        s = s.strip(": ")
+        dataList["Plan Name"] = s[:-1]
+        deductibleFound = False
+        deductibleLocation = -1
+        outOfPocketLocation = -1
+        copayLocation = -1
+        count = 0
+        notCoveredLine = 0
+        coveredLine = 0
+        def calcPremium(deductible):
+
+        #calculates the premium relative to deductible
+
+            deductible = int((dataList["Deductible"].split()[0][1:]).replace(',', ''))
+            if deductible < 700:
+                dataList["Premium Level"] = "Higher Premium Level"
+            elif deductible > 1400:
+                dataList["Premium Level"] = "Lower Premium Level"
             else:
-                dataList["Do I have to stay in network?"] = "Yes"
-                dataList["Do I need a referral to see a specialist?"] = "Yes"
-        
-        elif count==deductibleLocation:
-            dataList["Deductible"] = line[:-1]
-        
-        elif count==outOfPocketLocation:
-            dataList["OOP"] = line[:-1]
-    
-        elif count == copayLocation:
-            dataList["Copayment"] = line.split()[0]
-            if '$' not in dataList["Copayment"]:
-                dataList["Copayment"] = "$0"
-    
-        elif (not deductibleFound) and "deductible?" in line:
-            deductibleLocation = count+2
-            deductibleFound = True
-        
-        elif "limit for this plan?" in line:
-            outOfPocketLocation = count+2
-        
-        elif "% coinsurance" in line:
-            coinsuranceIndex = line.index("% coinsurance")
-        elif "injury or illness" in line:
-            copayLocation = count + 2
-    
-        elif "NOT Cover" in line:
-            notCoveredLine = count + 1
-        
-        elif "Other Covered Services" in line:
-            coveredLine = count
-        
-        else:
-            pass
-    
-        count += 1
-    
-    notCoveredList = []
-    lines = allLines[notCoveredLine:coveredLine]
-    for line in lines:
-        notCoveredList.append(line[2:-1])
-    dataList["NOT covered"] = notCoveredList
-    
-    calcPremium (dataList["Deductible"])
-    
-    return dataList
-    
-    txt.close()
+                dataList["Premium Level"] = "Medium Premium Level"
+
+        for line in allLines:
+            if "Plan Type" in line:
+                strList = line.split()
+                planType = strList[-1]
+                if planType == "HMO":
+                    dataList["Do I have to stay in network?"] = "Yes"
+                    dataList["Do I need a referral to see a specialist?"] = "Yes"
+                elif planType == "PPO":
+                    dataList["Do I have to stay in network?"] = "No"
+                    dataList["Do I need a referral to see a specialist?"] = "No"
+                elif planType == "EPO":
+                    dataList["Do I have to stay in network?"] = "Yes"
+                    dataList["Do I need a referral to see a specialist?"] = "No"
+                elif planType == "POS":
+                    dataList["Do I have to stay in network?"] = "No"
+                    dataList["Do I need a referral to see a specialist?"] = "Yes"
+                else:
+                    dataList["Do I have to stay in network?"] = "Yes"
+                    dataList["Do I need a referral to see a specialist?"] = "Yes"
+
+            elif count==deductibleLocation:
+                dataList["Deductible"] = line[:-1]
+
+            elif count==outOfPocketLocation:
+                dataList["OOP"] = line[:-1]
+
+            elif count == copayLocation:
+                dataList["Copayment"] = line.split()[0]
+                if '$' not in dataList["Copayment"]:
+                    dataList["Copayment"] = "$0"
+
+            elif (not deductibleFound) and "deductible?" in line:
+                deductibleLocation = count+2
+                deductibleFound = True
+
+            elif "limit for this plan?" in line:
+                outOfPocketLocation = count+2
+
+            elif "% coinsurance" in line:
+                coinsuranceIndex = line.index("% coinsurance")
+            elif "injury or illness" in line:
+                copayLocation = count + 2
+
+            elif "NOT Cover" in line:
+                notCoveredLine = count + 1
+
+            elif "Other Covered Services" in line:
+                coveredLine = count
+
+            else:
+                pass
+
+            count += 1
+
+        notCoveredList = []
+        lines = allLines[notCoveredLine:coveredLine]
+        for line in lines:
+            notCoveredList.append(line[2:-1])
+        dataList["NOT covered"] = notCoveredList
+
+        calcPremium (dataList["Deductible"])
+
+        print(dataList)
+        easyCompareList.append(dataList)
+
+        txt.close()
+    else:
+        break
+
+def getTable(dataLists):
+    table = []
+    keyList = []
+    for key in dataLists[0].keys():
+        keyList.append(key)
+        for dataList in dataLists:
+            keyList.append(dataList[key])
+        table.append(keyList)
+        keyList = []
+    return table
+
+def printTable(table):
+    for l in table:
+        print(l)
+
+printTable(getTable(easyCompareList))
+
 
 #close file
 
