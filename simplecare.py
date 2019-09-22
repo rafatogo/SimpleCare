11a6f7530f3561664e389e28d4e43585a999ce19
mini-mini-milo <53094803+mini-mini-milo@users.noreply.github.com>
2019-09-21 18:10:04 -0700

    Update simplecare.py

--- simplecare.py
+++ simplecare.py
@@ -14,29 +14,25 @@ Created on Sat Sep 21 12:03:57 2019
     #if search phrase in textfile:
         #print line(s) 
 """
+
 dataList = {}
-txt = imported text file
-allLines = txt.readLine()
-s = allLines[2]
-s.strip(": ")
-dataList["Plan Name"] = s
-with open('imported text file', 'r') as f:
-    for line in txt:
-        if "Plan Type: " in line:
-            string = str(line)
-            strList = string.split()
-            dataList["Plan Type"] = strList(len(strList)-1)
-        else:
-            pass
-with open('imported text file', 'r') as f:
-    data = f.readlines()
-line_no = data.index("Plan Type:")
-while text file != "":
-    if "deductible?" in text file:
+txt = open('Flex_Bronze.txt', 'r')
+allLines = txt.readlines()
+s = allLines[1]
+s = s.strip(": ")
+dataList["Plan Name"] = s[:-1]
+
+dataList["Plan Type"] = ""
+
+for line in txt:
+    if "Plan Type: " in line:
+        string = str(line)
+        strList = line.split(' ')
+        dataList["Plan Type"] = strList[-1]
+    
+    elif "deductible?" in line:
         deductibleLocation = line.index("deductible?")
-
         dataList["deductible"] = line[deductibleLocation: +1]
-
     else:
         pass
 
